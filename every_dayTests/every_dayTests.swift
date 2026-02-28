b929f858a3eabf62eb84bd4fcdc697bf5fff3994
Allen Russell <garyrusselljr@gmail.com>
2026-02-28 08:05:41 -0700

    Initial Commit

---
+++ every_dayTests/every_dayTests.swift
@@ -0,0 +1,16 @@
+//
+//  every_dayTests.swift
+//  every_dayTests
+//
+//  Created by Allen Russell on 2/28/26.
+//
+
+import Testing
+
+struct every_dayTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
